824594a4dd99d90c02700b81b33f96be4e905b24
Marcelearn <jaworskimarcel@gmail.com>
2022-11-13 11:15:52 +0100

    home and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import './App.css'
 import Home from './components/Home'
 
@@ -6,7 +6,19 @@ function App() {
 
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+      <header>
+     
+          <Link to="/">Home</Link>&nbsp;|&nbsp;
+          {/* <Link to="/products">Products</Link> */}
+      </header>
+        <h1>My little Quiz</h1>
+        <span>Check your knowledge!</span>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <h1>Home</h1>
+        <p>This is a quiz about GTML,CSS and JavaScript, that can be used to repeat all the basics that are needed in everday's life of  web developer.
+
+          Find out if you dnow all the details!
+        </p>
+        <button>Let's have fun!</button>
     </div>
   )
 }
