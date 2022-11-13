e00287bf3ab654f1450eaee83ed18210c846f6d1
Marcelearn <jaworskimarcel@gmail.com>
2022-11-13 12:34:53 +0100

    question

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import './App.css'
 import Home from './components/Home'
+import Questions from './components/Question'
 
 function App() {
 
@@ -9,13 +10,15 @@ function App() {
       <BrowserRouter>
       <header>
      
-          <Link to="/">Home</Link>&nbsp;|&nbsp;
+          <Link to="/">Home</Link>
           {/* <Link to="/products">Products</Link> */}
-      </header>
         <h1>My little Quiz</h1>
         <span>Check your knowledge!</span>
+      </header>
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/question" element={<Questions />}/>
+          
         </Routes>
       </BrowserRouter>
       
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
-        <p>This is a quiz about GTML,CSS and JavaScript, that can be used to repeat all the basics that are needed in everday's life of  web developer.
-
-          Find out if you dnow all the details!
-        </p>
-        <button>Let's have fun!</button>
+        <p>This is a quiz about GTML,CSS and JavaScript, that can be used to repeat all the basics that are needed in everday's life of  web developer.</p>
+        <p> Find out if you dnow all the details!</p>
+        <Link to={`/question/`}><button>Let's have fun!</button></Link>
     </div>
   )
 }
